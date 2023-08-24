73010c8d0de3534697a1b144e3924508234c6bed
Kamil Lewandowski <kamlew555@gmail.com>
2023-08-24 15:56:26 +0200

    create function openAccordion and add click event to btnEls variable

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const btnEls = document.querySelectorAll(".accordion__question");
+
+
+
+const openAccordion = (item) => {
+	
+};
+
+
+btnEls.forEach((item) => {
+	item.addEventListener("click", () => {
+	
+	});
+});
