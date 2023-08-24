e58eca7875fe74d673a4f6124dfeb2853163fd30
Kamil Lewandowski <kamlew555@gmail.com>
2023-08-24 18:00:28 +0200

    search and  save in variable accordion answare and toggle active class, search and save in arrowImgEl variable svg - arrow down

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const btnEls = document.querySelectorAll('.accordion__question')
 
 const openAccordion = (btn) => {
    
+    const answareEls = btn.nextElementSibling;
+		answareEls.classList.toggle("active");
+
+		const arrowImgEl = btn.querySelector(".accordion__rotate__arrow");
+
 }
 
 
