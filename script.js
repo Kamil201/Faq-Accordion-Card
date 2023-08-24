a14114195fa203f72eb64c0e8b8a3a75933f0db2
Kamil Lewandowski <kamlew555@gmail.com>
2023-08-24 18:11:53 +0200

    delete white spaces

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-const btnEls = document.querySelectorAll('.accordion__question')
-
+const btnEls = document.querySelectorAll(".accordion__question");
 
 const openAccordion = (btn) => {
-   
-    const answareEls = btn.nextElementSibling;
-		answareEls.classList.toggle("active");
+	const answareEls = btn.nextElementSibling;
+	answareEls.classList.toggle("active");
 
-		const arrowImgEl = btn.querySelector(".accordion__rotate__arrow");
+	const arrowImgEl = btn.querySelector(".accordion__rotate__arrow");
 
-        
-		if (answareEls.classList.contains("active")) {
-			answareEls.style.height = `${answareEls.scrollHeight}px`;
-			arrowImgEl.classList.add("rotate");
-		} else {
-			answareEls.style.height = "0px";
-			arrowImgEl.classList.remove("rotate");
-		}
+	if (answareEls.classList.contains("active")) {
+		answareEls.style.height = `${answareEls.scrollHeight}px`;
+		arrowImgEl.classList.add("rotate");
+	} else {
+		answareEls.style.height = "0px";
+		arrowImgEl.classList.remove("rotate");
+	}
+};
 
-}
-
-
-
-
-
-
-btnEls.forEach(btn => btn.addEventListener('click', () => openAccordion(btn)))
+btnEls.forEach((btn) =>
+	btn.addEventListener("click", () => openAccordion(btn))
+);
